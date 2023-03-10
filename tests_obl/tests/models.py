f23b8304dabffe434aa4611e41fcf06f4f707325
unknown <ruslanchik_nadyrov@mail.ru>
2023-03-10 22:12:56 +0600

    create_models

--- tests_obl/tests/models.py
+++ tests_obl/tests/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=15)
+    school = models.CharField(max_length=50)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=15)
+    school = models.CharField(max_length=50)
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=15)
+    school = models.CharField(max_length=50)
+    subject = models.CharField(max_length=15)
+
+
+class TestVersion(models.Model):
+    name = models.CharField(max_length=15)
+    test = models.CharField(max_length=50)
+
+
+class TestQuestion(models.Model):
+    test_version = models.CharField(max_length=15)
+    max_points = models.IntegerField(max_length=50)
